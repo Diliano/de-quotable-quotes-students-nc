382f1beca23812c7d09507489e9a11bd78623611
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-05 15:52:47 +0000

    refactor test name for test_s3.py

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -32,18 +32,38 @@ class TestWriteFileToS3:
     def test_writes_file_to_s3(self, mock_s3, mock_bucket):
         # Arrange
         test_filepath = "./tests/sonnet18.txt"
-        test_bucket = "test-bucket"
+        test_bucket_name = "test-bucket"
         test_key = "sonnet18.txt"
 
         expected = "File uploaded to bucket: test-bucket, on key: sonnet18.txt"
         # Act
-        result = write_file_to_s3(mock_s3, test_filepath, test_bucket, test_key)
-        response = mock_s3.list_objects_v2(Bucket=test_bucket)
+        result = write_file_to_s3(mock_s3, test_filepath, test_bucket_name, test_key)
+        response = mock_s3.list_objects_v2(Bucket=test_bucket_name)
         # Act
         assert result == expected
         assert response["Contents"][0]["Key"] == test_key
 
-    # def test_returns_error_message_given_invalid_path_to_file(self, mock_s3):
-    #     # Arrange
-    #     # Act
-    #     # Assert
+    def test_returns_error_message_given_invalid_path_to_file(
+        self, mock_s3, mock_bucket
+    ):
+        # Arrange
+        test_filepath = "not_a_real_filepath"
+        test_bucket_name = "test-bucket"
+        test_key = "sonnet18.txt"
+        # Act
+        result = write_file_to_s3(mock_s3, test_filepath, test_bucket_name, test_key)
+        # Assert
+        assert result == "No file found with path_to_file: not_a_real_filepath"
+
+    def test_returns_error_message_if_bucket_does_not_exist(self, mock_s3):
+        # Arrange
+        test_filepath = "./tests/sonnet18.txt"
+        test_bucket_name = "test-bucket"
+        test_key = "sonnet18.txt"
+        # Act
+        result = write_file_to_s3(mock_s3, test_filepath, test_bucket_name, test_key)
+        # Assert
+        assert (
+            result
+            == "Error: NoSuchBucket, Message: The specified bucket does not exist"
+        )
